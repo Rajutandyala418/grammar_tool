e0377b5641afdac74c4de86bf7728cf4d04ceada
TANDYALA RAJU <167282050+Rajutandyala418@users.noreply.github.com>
2025-08-30 20:35:06 +0530

    app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def speech_output():
         return jsonify({"audio_base64": ""})
         
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
 
 
 
